672140b5a38d9f9fa0779c2a9bfe9a57ab5e98c4
Rich_13 <106301413+Rich-13@users.noreply.github.com>
2024-05-15 14:41:48 +0800

    Update and rename Build.groovy to build.groovy

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,40 @@
+package org.devops
+
+
+// 使用函数简化构建步骤
+def buildAm64() {
+    echo 'Building for amd64'
+    buildImage('amd64')
+}
+
+def buildArm64() {
+    echo 'Building for arm64'
+    buildImage('arm64')
+}
+
+// 使用函数执行实际的镜像构建
+def buildImage(String arch) {
+    // 确保定义了 kubernetes agent 模板
+    def nodeLabel = (arch == 'amd64') ? 'kanikoamd' : 'kanikoarm'
+    agent { kubernetes { inheritFrom nodeLabel } }
+    
+    // ...这里放置实际的构建命令
+    unstash 'source-code' // 恢复之前存储的代码
+    container(nodeLabel) {
+        sh """
+        /kaniko/executor \
+              --context ${env.WORKSPACE}/${params.BUILD_DIRECTORY} \
+              --dockerfile ${params.BUILD_DIRECTORY}/Dockerfile \
+              --destination ${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/${env.JOB_NAME}:${env.VERSION_TAG}-${arch} \
+              --cache=true \
+              --cache-repo=${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/cache \
+              --skip-tls-verify \
+              --skip-unused-stages=true \
+              --custom-platform=linux/${arch} \
+              --build-arg BUILDKIT_INLINE_CACHE=1 \
+              --snapshotMode=redo \
+              --log-format=text \
+              --verbosity=info
+        """
+    }
+}
